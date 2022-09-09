7041c951a54d1cf1ed0023a35ced6df7c708b056
Mariana Alvares <m239142@dac.unicamp.br>
2022-09-09 19:50:27 -0300

    Comentário 6

--- PrimeiroExemplo/Create OlaMundo.cs
+++ PrimeiroExemplo/Create OlaMundo.cs
@@ -7,6 +7,7 @@ namespace INF099
             Console.WriteLine("Olá Alunos INF-099 !!!!");
 			Console.WriteLine("Alterações com o VS Code");
             // C5
+            // C6
         }
     }
 }
